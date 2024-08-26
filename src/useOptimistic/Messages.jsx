43d1736d3a5588bb407adb393f8daa00191d862a
Viet Nguyen T. <viet.nguyen@supremetech.vn>
2024-08-26 22:03:44 +0700

    Add case error for useOptimistic

--- src/useOptimistic/Messages.jsx
+++ src/useOptimistic/Messages.jsx
@@ -7,14 +7,23 @@ export default function Messages() {
     { text: 'Hello there!', sending: false, key: 1 },
   ]);
 
+  const [error, setError] = useState('');
+
   async function sendMessage(formData) {
-    const sentMessage = await deliverMessage(formData.get('message'));
-    setMessages((messages) => [...messages, { text: sentMessage }]);
+    try {
+      const sentMessage = await deliverMessage(formData.get('message'));
+      setMessages((messages) => [...messages, { text: sentMessage }]);
+      setError(''); // Clear any previous errors
+    } catch (err) {
+      setError('Error sending message');
+    }
   }
+
   return (
     <>
       <h1 className="title">useOptimistic</h1>
       <Thread messages={messages} sendMessage={sendMessage} />
+      {error && <p>{error}</p>}
     </>
   );
 }
--- src/util/deliverMessage.js
+++ src/util/deliverMessage.js
@@ -1,5 +1,11 @@
 /* eslint-disable no-unused-vars */
 export async function deliverMessage(message) {
   await new Promise((res) => setTimeout(res, 1000));
-  return message;
+
+  // Simulate 50% chance of success and 50% chance of error
+  if (Math.random() > 0.5) {
+    return message; // Success
+  } else {
+    throw new Error('Failed to deliver message'); // Simulated error
+  }
 }
