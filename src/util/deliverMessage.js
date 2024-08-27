9fa34d15b6bd090bdd7653e011972e2032aa3aad
Viet Nguyen T. <viet.nguyen@supremetech.vn>
2024-08-27 22:21:34 +0700

    Remove comment

--- src/util/deliverMessage.js
+++ src/util/deliverMessage.js
@@ -2,10 +2,9 @@
 export async function deliverMessage(message) {
   await new Promise((res) => setTimeout(res, 1000));
 
-  // Simulate 50% chance of success and 50% chance of error
   if (Math.random() > 0.5) {
-    return message; // Success
+    return message;
   } else {
-    throw new Error('Failed to deliver message'); // Simulated error
+    throw new Error('Failed to deliver message');
   }
 }
